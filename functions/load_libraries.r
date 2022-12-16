cb0e5442e4cb38be26a2eeb2d60cb6fd6431c2c4
penguin1312 <120418128+penguin1312@users.noreply.github.com>
2022-12-16 01:56:00 +0000

    Add files via upload

---
+++ functions/load_libraries.r
@@ -0,0 +1,11 @@
+#Title: Load libraries
+#Date generated: 16/12/2022
+#
+#
+#
+#Use library function to load the required packages
+library(palmerpenguins) #palmerpenguins dataset
+library(ggplot2) 
+suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(dplyr))
+library(svglite)
